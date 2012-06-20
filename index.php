6387659fce192bd4dc12419b55bde01a741c0fda
Lars Meinke <lars.meinke@hhu.de>
2012-06-20 17:19:55 +0200

    first navigation bar

--- index.php
+++ index.php
@@ -2,9 +2,15 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+        <title>Adminbereich</title>
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
     </head>
     <body>
-        
+    	<div id="leisteOben">
+            <nav>
+                <a href="#">Seiten</a>
+                <a href="#">Newsletter</a>
+            </nav>
+        </div>
     </body>
 </html>
