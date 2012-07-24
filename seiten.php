f8f44b3a63f7be90acd95af228259ad80c67c8c2
Lars Meinke <lars.meinke@hhu.de>
2012-07-24 14:40:32 +0200

    fixed Seiten db connection

--- seiten.php
+++ seiten.php
@@ -7,11 +7,10 @@ if( isset($_GET['action']) && $_GET['action'] == 'list' ) {
     $mysql = Helper::getMysqlConfig();
 
     $db = new MySQLi($mysql['host'], $mysql['user'], $mysql['pw'], $mysql['database']);
-    $sql = 'SELECT `id`, `name` FROM `?seiten`';
+    $sql = "SELECT `id`, `name` FROM `".$mysql['tablePrefix']."seiten`";
     $kommando = $db->prepare($sql);
-    $kommando->bind_result($id, $name);
-    $kommando->bind_param('s', $mysql['tablePrefix']);
     $kommando->execute();
+    $kommando->bind_result($id, $name);
     while($kommando->fetch()) {
         $tpl->load('seiten_body.html');
         $tpl->assign('name', $name);
