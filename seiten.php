13fee8ef178576a1bebe16aaca0a3ca292b279e2
Lars Meinke <lars.meinke@hhu.de>
2012-08-08 13:00:03 +0200

    added last changed to Seiten

--- seiten.php
+++ seiten.php
@@ -7,17 +7,22 @@ if( isset($_GET['action']) && $_GET['action'] == 'list' ) {
     $mysql = Helper::getMysqlConfig();
 
     $db = new MySQLi($mysql['host'], $mysql['user'], $mysql['pw'], $mysql['database']);
-    $sql = "SELECT `id`, `name` FROM `".$mysql['tablePrefix']."seiten`";
+    $sql = "SELECT `id`, `name`, `lastEdited` FROM `".$mysql['tablePrefix']."seiten`";
     $kommando = $db->prepare($sql);
     $kommando->execute();
-    $kommando->bind_result($id, $name);
+    $kommando->bind_result($id, $name, $lastEdited);
     while($kommando->fetch()) {
         $tpl->load('seiten_body.html');
         $tpl->assign('name', $name);
         $tpl->assign('link', $name);
-        $tpl->assign('bearbeiten', $id);
+        $tpl->assign('changed', $lastEdited);
+        $tpl->assign('id', $id);
         $this->tpl .= $tpl->out();
     }
     $db->close();
+    
+    $tpl->load("seiten_footer.html");
+    $this->tpl .= $tpl->out();
+
 }
 ?>
