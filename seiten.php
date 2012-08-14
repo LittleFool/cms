56aaa20bdc45fb7412841de80aa525e04eb41971
Lars Meinke <lars.meinke@hhu.de>
2012-08-14 17:29:43 +0200

    fixed edition of pages+

--- seiten.php
+++ seiten.php
@@ -37,18 +37,20 @@ if (isset($_GET['action']) && $_GET['action'] == 'list') {
 }
 
 // action == edit
-if (isset($_GET['action']) && $_GET['action'] == 'edit') {
+if (isset($_GET['action']) && isset($_GET['id']) && $_GET['action'] == 'edit' && $_GET['id'] >= 0) {
     $tpl = new Template();
     $tpl->load("seiten_edit.html");
 
     $mysql = Helper::getMysqlConfig();
+    $id = $_GET['id'];
 
     // get the current website
     $db = new MySQLi($mysql['host'], $mysql['user'], $mysql['pw'], $mysql['database']);
-    $sql = "SELECT `id`, `name`, `content` FROM `" . $mysql['tablePrefix'] . "seiten`";
+    $sql = "SELECT `name`, `content` FROM `" . $mysql['tablePrefix'] . "seiten` WHERE id=?";
     $kommando = $db->prepare($sql);
+    $kommando->bind_param('i', $id);
+    $kommando->bind_result($name, $content);
     $kommando->execute();
-    $kommando->bind_result($id, $name, $content);
     $kommando->fetch();
 
     // show the editor with the content
