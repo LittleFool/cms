54a445f0a88c3a7d689cf72902b88bbba7146779
Lars Meinke <lars.meinke@hhu.de>
2012-08-14 14:56:47 +0200

    addes save button

--- js/functions.js
+++ js/functions.js
@@ -9,7 +9,7 @@ $().ready(function() {
         plugins : "pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template",
 
         // Theme options
-        theme_advanced_buttons1 : "bold,italic,underline,strikethrough,justifyleft,justifycenter,justifyright,justifyfull,fontselect,fontsizeselect|,visualblocks,code",
+        theme_advanced_buttons1 : "save,|,bold,italic,underline,strikethrough,justifyleft,justifycenter,justifyright,justifyfull,fontselect,fontsizeselect,|,visualblocks,code",
         theme_advanced_buttons2 : "search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
         theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup",
         theme_advanced_buttons4 : "",
