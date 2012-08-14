d75228c68546bf715042700bb315d2cfbf465f29
Lars Meinke <lars.meinke@hhu.de>
2012-08-14 20:22:58 +0200

    changed date to german & fixed spelling

--- seiten.php
+++ seiten.php
@@ -11,7 +11,7 @@ if (isset($_GET['action']) && $_GET['action'] == 'list') {
 
     // list all websites
     $db = new MySQLi($mysql['host'], $mysql['user'], $mysql['pw'], $mysql['database']);
-    $sql = "SELECT `id`, `name`, `lastEdited` FROM `" . $mysql['tablePrefix'] . "seiten`";
+    $sql = "SELECT `id`, `name`, UNIX_TIMESTAMP(`lastEdited`) FROM `" . $mysql['tablePrefix'] . "seiten`";
     $kommando = $db->prepare($sql);
     $kommando->execute();
     $kommando->bind_result($id, $name, $lastEdited);
@@ -21,6 +21,7 @@ if (isset($_GET['action']) && $_GET['action'] == 'list') {
             case 1: $line = 'odd'; break;
         }
         $i++;
+        $lastEdited = date("d.m.Y G:H:s", $lastEdited);
         
         $tpl->load('seiten_body.html');
         $tpl->assign('name', $name);
